d31be6d2e4e702bd732dacb33389024a0f66f28f
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2025-06-07 06:39:19 +0900

    レイアウトの実装

--- pages/member/memberList.vue
+++ pages/member/memberList.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import type { Member } from "@/interfaces";
+
+definePageMeta({
+  layout: "member",
+});
 // 会員情報リストをステートから取得
 const memberList = useState<Map<number, Member>>("memberList");
 </script>
 
 <template>
-  <h1>会員管理</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li><NuxtLink v-bind:to="{ name: 'index' }">TOP</NuxtLink></li>
+      <li>会員リスト</li>
+    </ul>
+  </nav>
   <section>
-    <nav id="breadcrumbs">
-      <ul>
-        <li><NuxtLink v-bind:to="{ name: 'index' }">TOP</NuxtLink></li>
-        <li>会員リスト</li>
-      </ul>
-    </nav>
     <h2>会員リスト</h2>
     <p>
       新規登録は<NuxtLink v-bind:to="{ name: 'member-memberAdd' }"
